e722a2ec9d216d39d3f3b0a4f83663ee882f07ff
favourchidinma <fcakagha9943@gmail.com>
2022-11-16 08:20:16 -0800

    main.cj

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
- * main - entry point
+ * main - program entry point
  * @ac: arg count
  * @av: arg vector
- *
  * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
